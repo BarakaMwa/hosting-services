4ac88be2c2ab2abdf1bbd1a6ab10dc5be58d7208
Baraka Mwang'amba <baraka.mwangamba@gmail.com>
2023-06-14 16:20:37 +0200

    MOdernixer

--- users/register/index.php
+++ users/register/index.php
@@ -63,14 +63,14 @@ if (isset($_POST['btn-signup'])) {
 <head>
     <title>Signup | Coding Cage</title>
     <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-    <link href="assets/styles.css" rel="stylesheet" media="screen">
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+    <link href="../assets/styles.css" rel="stylesheet" media="screen">
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <script src="../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 </head>
 <body id="login">
 <div class="container">
@@ -87,7 +87,7 @@ if (isset($_POST['btn-signup'])) {
     </form>
 
 </div> <!-- /container -->
-<script src="vendors/jquery-1.9.1.min.js"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="../vendors/jquery-1.9.1.min.js"></script>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>

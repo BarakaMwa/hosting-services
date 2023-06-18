2221d8d3143aacf43f0c7e7e9c7f9630b8bab3a0
Baraka Mwang'amba <baraka.mwangamba@gmail.com>
2023-06-19 00:05:23 +0200

    Get return

--- api/login/index.php
+++ api/login/index.php
@@ -9,7 +9,7 @@ if ($user_login->is_logged_in() != "") {
     $response['status'] = true;
     $response['message'] = "Logged In";
 //    $user_login->redirect('../home-page/index.php');
-    echo json_encode($response, JSON_THROW_ON_ERROR);
+    return json_encode($response, JSON_THROW_ON_ERROR);
 }
 
 if ($_SESSION["REQUEST_METHOD"] == "POST") {
@@ -29,7 +29,7 @@ if ($_SESSION["REQUEST_METHOD"] == "POST") {
 }else{
     $response["status"] = false;
     $response["message"] = "Invalid Requests";
-    echo json_encode($response, JSON_THROW_ON_ERROR);
+    return json_encode($response, JSON_THROW_ON_ERROR);
 }
 ?>
 

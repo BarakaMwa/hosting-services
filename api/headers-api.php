29f747f086d98b52a31d45e55c3d75f04910736e
Baraka Mwang'amba <baraka.mwangamba@gmail.com>
2023-06-19 01:58:32 +0200

    headers-api.php added

--- api/headers-api.php
+++ api/headers-api.php
@@ -1,5 +1,5 @@
 <?php
-//
-//header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Credentials: true");
-//header("Content-type:application/json;charset=utf-8");
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Content-type:application/json;charset=utf-8");

0592f221d156404f805afac1e6909a978d408f1c
Baraka Mwang'amba <baraka.mwangamba@gmail.com>
2023-06-29 13:35:20 +0200

    util added new function

--- api/constants/Utils.php
+++ api/constants/Utils.php
@@ -1,11 +1,33 @@
 <?php
 
-class Utils{
+class Utils
+{
 
+    /**
+     * @param array $data
+     * @return string
+     */
     public function cleanString($data): string
     {
         $data = trim($data);
         $data = stripslashes($data);
         return htmlspecialchars($data);
     }
+
+    /**
+     * @param int $length
+     * @return string
+     * @throws Exception
+     */
+    public function generateRandomString(int $length = 16): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return str_shuffle($randomString);
+    }
+
 }
